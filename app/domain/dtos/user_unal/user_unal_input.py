ff7341a42955f3e581f8f95a19e1faf7cc0e2ea3
MuelitasVill <mmartinezve@unal.edu.co>
2025-11-10 23:04:57 -0500

    initial commit

---
+++ app/domain/dtos/user_unal/user_unal_input.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import date
+
+
+class UserUnalInput(BaseModel):
+    email_unal: EmailStr
+    document: Optional[str] = None
+    name: Optional[str] = None
+    lastname: Optional[str] = None
+    full_name: Optional[str] = None
+    gender: Optional[str] = None
+    birth_date: Optional[date] = None
+    headquarters: Optional[str] = None
